4cbcb22f95017437c46d1c2ec4d85bd7bbff1254
Sage Strack <sagecs@live.unc.edugit config --global user.name Sage>
2023-02-05 20:48:15 -0500

    ex02

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,4 +1,4 @@
-"""One-Shot Wordle Exercise"""
+"""One-Shot Wordle Exercise."""
 __author__ = "730331072"
 
 secret_word: str = "python"
@@ -13,25 +13,22 @@ chr_contain: bool = False
 alt_idx: int = 0
 
 if len(guess) != len(secret_word):
-    guess: str = input(f"That was not {str(len(secret_word))} letters! Try again: ")
-if len(guess) == len(secret_word):
+    guess = input(f"That was not {str(len(secret_word))} letters! Try again: ")
+else:
     while guess_idx < len(secret_word):
         if guess[guess_idx] == secret_word[guess_idx]:
             emoji = emoji + GREEN_BOX
         else:
             while chr_contain == False and guess_idx < len(secret_word) and alt_idx < len(secret_word):
                 if guess[guess_idx] == secret_word[alt_idx]:
-                    chr_contain = True
+                    chr_contain == True
+                    emoji = emoji + YELLOW_BOX
                 else:
                     alt_idx = alt_idx + 1
-            if chr_contain == True:
-                emoji = emoji + YELLOW_BOX
-            else:
-                emoji = emoji + WHITE_BOX
+                    emoji = emoji + WHITE_BOX
         guess_idx = guess_idx + 1
     print(emoji)
     if guess != secret_word:
-        print ("Not quite. Play again soon!")
+        print("Not quite. Play again soon!")
     else:
-        print ("Woo! You got it!")
-
+        print("Woo! You got it!")
